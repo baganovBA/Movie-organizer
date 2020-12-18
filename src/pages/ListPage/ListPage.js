bd08afba80c260bb30ec6911aa21b5c08e55747d
boris Baganov <bbaganov@gmail.com>
2020-12-18 19:36:53 +0300

    Prentation verion

--- src/pages/ListPage/ListPage.js
+++ src/pages/ListPage/ListPage.js
@@ -46,7 +46,7 @@ class ListPage extends Component {
                     {this.state.renderFilms.map((item) => {
                         return (
                             <li key={item.imdbID}>
-                                <a href={`https://www.imdb.com/title/${item.imdbID}/`} target="_blank">{item.Title} ({item.Year})</a>
+                                <a href={`https://www.imdb.com/title/${item.imdbID}/`} target="_blank" rel="noopener noreferrer">{item.Title} ({item.Year})</a>
                             </li>
                         );
                     })}
